017b4d73b6e58e9d5e10da7577736f5c38c8376d
sohamsinh31 <sohamb2019@gmail.com>
2023-06-08 22:29:25 +0530

    UI changes

--- login.php
+++ login.php
@@ -19,57 +19,80 @@ if (isset($parts['query'])) {
 <html lang="en">
 
 <head>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./js/jquery-3.6.0.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./fullcalendar/lib/main.min.js"></script>
     <link id="stylesheet" rel="stylesheet" type="text/css" href="styles.css" />
 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-        body {
-            background-image: url('city.jpg');
-            background-size: cover;
-        }
-    </style>
 </head>
 
 <body>
-    <div class="upload">
-        <div class="col-lg-6">
-            <h2>Log in form</h2>
-            <form action="validation.php" method="post" enctype="multipart/form-data">
-                <div class="form-groop">
-                    <label>email</label>
-                    <br>
-                    <input
-                        style="width:80%;float:left;border-bottom: 2px solid white;background:none;border-style: none none solid;"
-                        type="text" name="user">
-                    <br>
-                </div>
+    <section class="vh-100 gradient-custom">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
 
-                <div class="form-groop">
-                    <br>
-                    <label>password</label>
-                    <br>
-                    <input
-                        style="width:80%;float:left;border-bottom: 2px solid white;background:none;border-style: none none solid;"
-                        type="password" name="password">
+                            <div class="mb-md-5 mt-md-4 pb-5">
+
+                                <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                                <p class="text-white-50 mb-5">Please enter your login and password!</p>
+                                <form action="validation.php" method="post" enctype="multipart/form-data">
+
+                                    <div class="form-outline form-white mb-4">
+                                        <input type="email" id="typeEmailX" name="user" style="color:black;" class="form-control form-control-lg" />
+                                        <label class="form-label" for="typeEmailX">Email</label>
+                                    </div>
+
+                                    <div class="form-outline form-white mb-4">
+                                        <input type="password" id="typePasswordX" name="password"
+                                            class="form-control form-control-lg" style="color:black;" />
+                                        <label class="form-label" for="typePasswordX">Password</label>
+                                    </div>
+                                    <input type="hidden" name="next" value="<?php echo $next ?>">
+
+                                    <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot
+                                            password?</a>
+                                    </p>
+
+                                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+                                    <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                                        <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                                        <a href="#!" class="text-white"><i
+                                                class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                                        <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                                    </div>
+
+                            </div>
+                            </form>
+
+                            <div>
+                                <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign
+                                        Up</a>
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
-                <input type="hidden" name="next" value=<?php echo $next ?>>
-                <br>
-                <br>
-                <span><a style="color:gold;" href="signup.php">Not logged in yet click to Sign up</a></span><br>
-                <button style="background-color:blue;border-radius:12px;width:20%;color:white;" onclick="return foo();"
-                    type="submit" name="submit" class="btn btn-primary">Login</button>
-                <?php
-                $message = $_GET['message'];
-                if ($message) {
-                    echo "<h3>Incorrect username or password</h3>";
-                }
-                ?>
-            </form>
+            </div>
         </div>
+    </section>
+    <?php
+    $message = $_GET['message'];
+    if ($message) {
+        echo "<h3>Incorrect username or password</h3>";
+    }
+    ?>
+    </form>
+    </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
